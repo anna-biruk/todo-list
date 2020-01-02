578d5a5425cfd549836986bc01b5ae64058b5fbc
Anna <anna.biryuk.97@mail.ru>
2020-01-02 16:40:15 +0300

    Updates

---
+++ precache-manifest.af737bb56260cc36a6df787c9e05aebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f91bf753381d5a02c71aa3ff6419db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76253d834cfdbf942f55",
+    "url": "/static/css/main.4bac506f.chunk.css"
+  },
+  {
+    "revision": "f6ad6b3d58700e078293",
+    "url": "/static/js/2.8599cb35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8599cb35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76253d834cfdbf942f55",
+    "url": "/static/js/main.bfb23126.chunk.js"
+  },
+  {
+    "revision": "03a0013317e6f2a8a99d",
+    "url": "/static/js/runtime-main.5c7f07e5.js"
+  }
+]);
